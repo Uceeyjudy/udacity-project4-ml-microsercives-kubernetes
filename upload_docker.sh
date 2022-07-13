a8b4d849f33229ac26aa9877db5f45ca9418a614
Uceeyjudy <ucjudith@gmail.com>
2022-07-13 20:54:16 +0100

    docker out.txt and upload docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=juditheze/ml-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u juditheze
+docker image tag ml-project $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
