5a6edb734e5b2f2aa6b72641eed2bb99afc15690
Uceeyjudy <ucjudith@gmail.com>
2022-07-14 01:45:21 +0100

    changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=juditheze/ml-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-project --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-project 8080:80
